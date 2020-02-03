381dfbf9e81ea1263841130e6eae8ec8d874be5f
Val <valentin.belissen@limsi.fr>
2020-02-03 15:31:35 +0100

    First commit

---
+++ setup.py
@@ -0,0 +1,10 @@
+from setuptools import find_packages, setup
+
+setup(
+    name='src',
+    packages=find_packages(),
+    version='0.1.0',
+    description='Automatic recognition of SL structures in RGB videos',
+    author='LIMSI',
+    license='MIT',
+)
