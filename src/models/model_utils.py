7ce8b70c50ac4a1c6ba4dff60bd7d2bce1283835
Valentin Belissen <valentin.belissen@limsi.fr>
2020-06-09 09:13:09 +0200

    argparse

--- src/models/model_utils.py
+++ src/models/model_utils.py
@@ -32,7 +32,7 @@ def recallKtest(y_true, y_pred):
     TP = K.equal(y_true_class, y_pred_class) #this is also something I use a lot for gathering elements
     #FP = (1-y_true_class) * y_pred_class
     #FN = y_true_class * (1-y_pred_class)
-    nonzero_true = K.any(K.not_equal(y_true_class, 0.0), axis=-1)
+    nonzero_true = K.any(K.not_equal(y_true_class, 0), axis=-1)
     #true_positives = K.sum(K.round(K.clip(y_true_class * y_pred_class, 0, 1)))
     #possible_positives = K.sum(K.round(K.clip(y_true_class, 0, 1)))
     #recall = true_positives / (possible_positives + K.epsilon())
@@ -45,7 +45,7 @@ def precisionKtest(y_true, y_pred):
     TP = K.equal(y_true_class, y_pred_class) #this is also something I use a lot for gathering elements
     #FP = (1-y_true_class) * y_pred_class
     #FN = y_true_class * (1-y_pred_class)
-    nonzero_pred = K.any(K.not_equal(y_pred_class, 0.0), axis=-1)
+    nonzero_pred = K.any(K.not_equal(y_pred_class, 0), axis=-1)
     #true_positives = K.sum(K.round(K.clip(y_true_class * y_pred_class, 0, 1)))
     #possible_positives = K.sum(K.round(K.clip(y_true_class, 0, 1)))
     #recall = true_positives / (possible_positives + K.epsilon())
