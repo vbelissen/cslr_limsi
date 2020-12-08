6d742240dca3de4b3060a1f2535405cdf2289896
Valentin Belissen <valentin.belissen@limsi.fr>
2020-12-08 17:37:13 +0100

    update

--- src/recognitionUniqueDictaSignFromScript.py
+++ src/recognitionUniqueDictaSignFromScript.py
@@ -282,7 +282,7 @@ features_train, annot_train = get_data_concatenated(corpus=corpus,
                                                     video_indices=idxTrain,
                                                     separation=separation,
                                                     features_dict=features_dict,
-                                                    features_type=features_type)
+                                                    features_type=inputFeaturesFrames)
 features_valid, annot_valid = get_data_concatenated(corpus=corpus,
                                                     output_form=output_form,
                                                     output_names_final=[outputName],
@@ -290,7 +290,7 @@ features_valid, annot_valid = get_data_concatenated(corpus=corpus,
                                                     video_indices=idxValid,
                                                     separation=separation,
                                                     features_dict=features_dict,
-                                                    features_type=features_type)
+                                                    features_type=inputFeaturesFrames)
 features_test, annot_test   = get_data_concatenated(corpus=corpus,
                                                     output_form=output_form,
                                                     output_names_final=[outputName],
@@ -298,7 +298,7 @@ features_test, annot_test   = get_data_concatenated(corpus=corpus,
                                                     video_indices=idxTest,
                                                     separation=separation,
                                                     features_dict=features_dict,
-                                                    features_type=features_type)
+                                                    features_type=inputFeaturesFrames)
 
 #print(len(features_train))
 #print(features_train[0].shape)
